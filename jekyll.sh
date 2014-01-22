f36e98209bf10a13000dd625fe8069f63c1cdfdf
Matt Gautreau <mgaut72@gmail.com>
2014-01-22 13:17:43 -0700

    rm container after use, ignore the temp container id file I make

--- jekyll.sh
+++ jekyll.sh
@@ -1,1 +1,4 @@
-sudo docker run -i -t -p 4000:4000 -v ~/documents/semester6/cryptography/CryptoAlgebra:/data ghp2 "$@"
+sudo docker run -i -cidfile="./cid.txt" -t -p 4000:4000 -v ~/documents/semester6/cryptography/CryptoAlgebra:/data ghp2 "$@"
+sudo docker stop $(cat ./cid.txt)
+sudo docker rm $(cat ./cid.txt)
+sudo rm ./cid.txt
