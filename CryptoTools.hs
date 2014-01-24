1557bb75ae3ddefe37d6b5e6e2cb9e7b9128bab6
Matt Gautreau <mgaut72@gmail.com>
2014-01-24 12:25:10 -0700

    add faster phi function and factors function from haskell 99 problems

--- CryptoTools.hs
+++ CryptoTools.hs
@@ -1,5 +1,8 @@
 module CryptoTools where
 import Data.Char (isAlpha, toUpper)
+import Data.List (nub)
+import Data.Ratio
+
 
 -- converts characters
 -- A B .. Z
@@ -32,10 +35,19 @@ englishFrequencies = [ 0.082, 0.015, 0.028, 0.043, 0.127, 0.022, 0.020, 0.061
 -- get the frequencies of letters in a given string
 letterFreq :: String -> [Double]
 letterFreq s = [ normalize . countC c $ (map toUpper s) | c <- ['A' .. 'Z']]
-    where countC c  = length . filter (== c) 
+    where countC c  = length . filter (== c)
           normalize = (/ fromIntegral (length s)) . fromIntegral
 
 -- Euler phi function, nieve
 phi' :: Int -> Int
 phi' x = length [ a | a <- [1..(x-1)], gcd a x == 1 ]
 
+phi :: (Integral a) => a -> a
+phi 1 = 1
+phi n = numerator ratio `div` denominator ratio
+ where ratio = foldl (\acc x -> acc * (1 - (1 % x)))
+                        (n % 1) $ nub (factors n)
+factors 1 = []
+factors n = let divisors = dropWhile ((/= 0) . mod n) [2 .. ceiling $ sqrt $ fromIntegral n]
+               in let prime = if null divisors then n else head divisors
+                      in (prime :) $ factors $ div n prime
